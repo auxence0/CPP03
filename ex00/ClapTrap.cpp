cf0d1723512c4aa9edd034cae29bac9e1f959356
auxence0 <sauvagetauxence@gmail.com>
2026-08-12 14:49:29 +0200

    ex00

---
+++ ex00/ClapTrap.cpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asauvage <asauvage@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/12 12:06:13 by asauvage          #+#    #+#             */
+/*   Updated: 2026/08/12 14:29:36 by asauvage         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap( std::string name ): name_(name), hit_points_(10), energy_points_(10), attack_damage_(0){
+	std::cout << "Constructor called\n";
+	return ;
+}
+
+ClapTrap::ClapTrap( const ClapTrap& obj ) {
+	std::cout << "Copy constructor called\n";
+	*this = obj;
+	return ;
+}
+
+ClapTrap::~ClapTrap() {
+	std::cout << "Destructor called\n";
+	return ;
+}
+
+ClapTrap&	ClapTrap::operator=( const ClapTrap& rhs ) {
+	this->name_ = rhs.name_;
+	this->attack_damage_ = rhs.attack_damage_;
+	this->energy_points_ = rhs.energy_points_;
+	this->hit_points_ = rhs.hit_points_;
+	return *this;
+}
+
+void	ClapTrap::attack( const std::string& target) {
+	if (energy_points_ > 0) {
+		std::cout << "ClapTrap " + name_ + " attacks " + target + ", causing " << attack_damage_ << " points of damage!\n";
+		energy_points_--;
+	}
+	else
+		std::cout << "Need energy point\n";
+	return ;
+}
+
+void	ClapTrap::takeDamage( unsigned int amount ) {
+	std::cout << name_ + " took " << amount << " damage ";
+	hit_points_ -= amount;
+	if (hit_points_ < 0)
+		hit_points_ = 0;
+	std::cout << "He's now at " << hit_points_ << "\n";
+	return ;
+}
+
+void	ClapTrap::beRepaired( unsigned int amount ) {
+	if (energy_points_ > 0) {
+		std::cout << name_ + " gained " << amount << " hit points\n";
+		energy_points_--;
+	}
+	else
+		std::cout << "Need energy point\n";
+	return ;
+}
---
+++ ex00/main.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asauvage <asauvage@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/12 14:11:21 by asauvage          #+#    #+#             */
+/*   Updated: 2026/08/12 14:48:14 by asauvage         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int	main(void) {
+	ClapTrap	x ( "tendancieux" );
+	ClapTrap	y ( "tandori" );
+	ClapTrap	z ( "Peripetie" );
+
+	std::cout << "\n\nSimple test\n\n";
+	x.attack("tandori");
+	y.takeDamage(4);
+	y.beRepaired(15);
+
+	y.attack("Peripetie");
+	z.takeDamage(100);
+
+	std::cout << "\n\nLose all energy points\n\n";
+	for (int i = 0; i < 10; ++i) {
+		z.beRepaired(10);
+	}
+	z.attack("failed");
+
+	std::cout << "\n\nCpy constructor and assignement test\n\n";
+	ClapTrap	cpy_construct(y);
+	ClapTrap	celuila("ici");
+
+	celuila = cpy_construct;
+
+	celuila.attack("Mr Biasoni");
+
+	return 0;
+}
