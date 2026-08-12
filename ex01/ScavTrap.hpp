61d90fcc5201c5a587754f8f7601a3f877d50272
auxence0 <sauvagetauxence@gmail.com>
2026-08-12 16:31:13 +0200

    banane

---
+++ ex01/ScavTrap.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asauvage <asauvage@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/12 14:51:47 by asauvage          #+#    #+#             */
+/*   Updated: 2026/08/12 15:59:00 by asauvage         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include "ClapTrap.hpp"
+
+class	ScavTrap: public ClapTrap {
+	public:
+		
+};
+
+#endif
